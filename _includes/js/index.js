378d07b231c67b2e0bbea49d35734667dc6ecadb
abeltranes <abeltranes@yahoo.es>
2019-02-28 12:44:08 +0100

    borrando intentos

--- _includes/js/index.js
+++
@@ -1,9 +0,0 @@
-function changeClass()
-{
- document.getElementById("open-hide").classList.toggle('show');
-}
-
-window.onload = function() 
-{
-  document.getElementById("open-hide").addEventListener( 'click' , changeClass ); 
-}
