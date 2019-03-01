3f85c5a2e6c37756cf60283d0f73124c54dfe286
abeltranes <abeltranes@yahoo.es>
2019-03-01 12:37:19 +0100

    Update just-the-docs.js

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -1,11 +1,3 @@
-// Ocultacion del menu
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
 
 // Event handling
 
